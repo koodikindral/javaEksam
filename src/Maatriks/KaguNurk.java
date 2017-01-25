7723edb131ee7107761231c12da4332c9cc4c0cb
Gert Vesterberg <gert@estsoft.ee>
2017-01-25 13:29:24 +0200

    Maatriks: KaguNurk

--- src/Maatriks/KaguNurk.java
+++ src/Maatriks/KaguNurk.java
@@ -21,6 +21,34 @@ import java.util.Arrays;
  */
 public class KaguNurk {
 
+    public static void main(String[] args) {
+        final int suurus = 9;
+        int[][] maatriks = new int[suurus][suurus];
+        int number = 0;
+
+        for (int i = 0; i < maatriks.length; i++) {
+            for (int j = 0; j < maatriks.length; j++) {
+                maatriks[i][j] = i == j ? ++number : j >= i ? number : j+1;
+            }
+        }
+        
+        // Pöörame maatriksit 180 kraadi võrra kellaosuti liikumise suunas
+        printMaatriks(pooraMaatriksit(pooraMaatriksit(maatriks)));
+    }
+
+    // Pöörab maatriksit kellaosuti liikumise suunas 90 kraadi.
+    static int[][] pooraMaatriksit(int[][] maatriks) {
+        final int M = maatriks.length;
+        final int N = maatriks[0].length;
+        int[][] ret = new int[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M-1-r] = maatriks[r][c];
+            }
+        }
+        return ret;
+    }
+
     // Lihtsalt abiline meetod, et maatriksit välja printida
     private static void printMaatriks(int[][] laud) {
         for (int i = 0; i < laud.length; i++) {
