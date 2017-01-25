16ca3d8365a4e1884c0f39ae197fcb9e17e8d37d
Gert Vesterberg <gert@estsoft.ee>
2017-01-25 13:43:36 +0200

    Maatriks: Kikilips

--- src/Maatriks/Kikilips.java
+++ src/Maatriks/Kikilips.java
@@ -20,8 +20,41 @@ import java.util.Arrays;
  */
 public class Kikilips {
 
+    public static void main(String[] args) {
+        final int suurus = 9;
+        char[][] maatriks = new char[suurus][suurus];
+
+        for (int i = 0; i < maatriks.length; i++) {
+            for (int j = 0; j < maatriks.length; j++) {
+
+                if (i > suurus / 2) {
+                    // Maatriksi alumine poolt
+                    maatriks[i][j] = j >= i + 1 || suurus - i > j + 1 ? '.' : '0';
+                } else {
+                    // Maatriksi ülemine pool
+                    maatriks[i][j] = j >= i && suurus - i > j ? '0' : '.';
+                }
+            }
+        }
+
+        printMaatriks(pooraMaatriksit(maatriks));
+    }
+
+    // Pöörab maatriksit kellaosuti liikumise suunas 90 kraadi.
+    static char[][] pooraMaatriksit(char[][] maatriks) {
+        final int M = maatriks.length;
+        final int N = maatriks[0].length;
+        char[][] ret = new char[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M-1-r] = maatriks[r][c];
+            }
+        }
+        return ret;
+    }
+
     // Lihtsalt abiline meetod, et maatriksit välja printida
-    private static void printMaatriks(int[][] laud) {
+    private static void printMaatriks(char[][] laud) {
         for (int i = 0; i < laud.length; i++) {
             System.out.println(Arrays.toString(laud[i]));
         }
