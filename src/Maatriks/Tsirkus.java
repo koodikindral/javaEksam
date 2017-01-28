3da54cc4159a9680f120ee762a8109d99812bc41
Gert Vesterberg <gert@estsoft.ee>
2017-01-28 16:37:28 +0200

    Maatriks: Tsirkus

--- src/Maatriks/Tsirkus.java
+++ src/Maatriks/Tsirkus.java
@@ -16,6 +16,36 @@ import java.util.Arrays;
  */
 public class Tsirkus {
 
+    public static void main(String[] args) {
+        final int suurus = 10;
+        int[][] maatriks = new int[suurus][suurus];
+
+        for (int i = 0; i < maatriks.length; i++) {
+            for (int j = 0; j < maatriks.length; j++) {
+                if (i % 2 == 0) {
+                    maatriks[i][j] =  (i + 1) * suurus - j;
+                } else {
+                    maatriks[i][j] = (i + 1) * suurus - suurus + j + 1;
+                }
+            }
+        }
+
+        printMaatriks(pooraMaatriksit(pooraMaatriksit(maatriks)));
+    }
+
+    // Pöörab maatriksit kellaosuti liikumise suunas 90 kraadi.
+    static int[][] pooraMaatriksit(int[][] maatriks) {
+        final int M = maatriks.length;
+        final int N = maatriks[0].length;
+        int[][] ret = new int[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M-1-r] = maatriks[r][c];
+            }
+        }
+        return ret;
+    }
+
     // Lihtsalt abiline meetod, et maatriksit välja printida
     private static void printMaatriks(int[][] laud) {
         for (int i = 0; i < laud.length; i++) {
